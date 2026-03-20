8462ae477775f1c440172bd414105f892833f683
SaraPasolini <saraalmeida3606@gmail.com>
2026-03-20 00:20:25 -0300

    Mudanças no codigo

--- Perguntas/pergunta14/Program.cs
+++ Perguntas/pergunta14/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Carro
+{
+    public string Cor { get; set; }
+    public int Ano { get; set; }
+
+    public Carro(string cor, int ano)
+    {
+        Cor = cor;
+        Ano = ano;
+    }
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Cor: {Cor}, Ano: {Ano}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+       
+        Carro carro1 = new Carro("Preto", 2023);
+        Carro carro2 = new Carro("Branco", 2022);
+
+        carro1.ExibirDados();
+        carro2.ExibirDados();
+    }
+}
